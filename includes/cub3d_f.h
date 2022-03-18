8e5035a72a01c16af02719a187e20e57516d6c5a
Mathias MAURAISIN <mathias.mrsn@gmail.com>
2022-03-18 10:36:04 +0100

    define mouse_move

--- includes/cub3d_f.h
+++ includes/cub3d_f.h
@@ -6,7 +6,7 @@
 /*   By: mamaurai <mamaurai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 15:50:05 by mamaurai          #+#    #+#             */
-/*   Updated: 2022/03/17 16:13:21 by mamaurai         ###   ########.fr       */
+/*   Updated: 2022/03/18 10:35:28 by mamaurai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,7 +84,10 @@ double				__pythagore__(double x1, double y1, double x2, double y2);
 void				moves(t_cub *s);
 int					key_handler(int key, t_cub *s);
 int					key_release(int keycode, t_cub *s);
-void				mouse_move(t_cub *s);
+
+# if defined(__APPLE__) && defined(__MACH__)
+	void				mouse_move(t_cub *s);
+# endif
 
 /*
 **	QUIT
